1c9c8a49d7a118705234e8bb8ed4d23bf0dc351c
Gillian Wenhold <gwenhold@mac.com>
2019-10-04 22:10:48 -0400

    Done.

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,20 @@
 class FakeArray
+  attr_accessor :array
+  def initialize(*args)
+    @array = args
+  end
 
-  # you'll need a splat in this class somewhere
+  def [](index)
+    @array[index]
+  end
 
+  def first
+    @array[0]
+  end
 
-
+  def each
+    @array.each do |i|
+      yield(i)
+    end
+  end
 end
